78cd5e2300c204b1a89ad3b775aaa9ecadfba090
jinjuan <943991963@qq.com>
2020-05-24 21:38:36 +0800

    搭建ts

---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>你好啊{{name}}</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data () {
+        return{
+            name:"TypeScript"
+        }
+    }
+})
+</script>
